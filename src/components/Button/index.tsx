a2799ca7d1597fb2ae1b45db44ab3c3e9089b620
Ivan Saldano <ivanms1@gmail.com>
2021-07-25 01:44:30 +0900

    create button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,21 @@
+import React from "react";
+import classNames from "classnames";
+
+import styles from "./Button.module.scss";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  icon: any;
 }
 
-function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+function Button({ children, className, icon = null, ...props }: ButtonProps) {
+  const Icon = icon;
+  return (
+    <button className={classNames(styles.Button, className)} {...props}>
+      {icon && <Icon className={styles.Icon} />}
+      {children}
+    </button>
+  );
 }
 
 export default Button;
